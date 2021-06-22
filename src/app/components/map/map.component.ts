7307ec7a0828503cc41018201a5f371cee4ada30
mohammed-eddaghal <mohammed_eddaghal@um5.ac.ma>
2021-06-22 23:35:42 +0100

    LGTM

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -10,11 +10,22 @@ import 'leaflet-routing-machine';
 export class MapComponent implements OnInit {
 
   private map: L.Map;
+  private  routes;
+  private camionPos: L.LatLngExpression = [34.02160888075479, -6.853381776506305];
   private centroid: L.LatLngExpression = [34.01908140063287, -6.849192298632323]; //
 
-  // retrieve from https://gist.github.com/ThomasG77/61fa02b35abf4b971390
-  smallIcon = new L.Icon({
+    icongarbege = new L.Icon({
     iconUrl: 'https://image.flaticon.com/icons/png/512/684/684113.png',
+    iconRetinaUrl: 'https://image.flaticon.com/icons/png/512/684/684113.png',
+    iconSize:    [35, 40],
+    iconAnchor:  [12, 40],
+    popupAnchor: [3, -40],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    shadowSize:  [41, 41]
+  });
+
+  iconCamion = new L.Icon({
+    iconUrl: '/assets/16348.png',
     iconRetinaUrl: 'https://image.flaticon.com/icons/png/512/684/684113.png',
     iconSize:    [35, 40],
     iconAnchor:  [12, 40],
@@ -36,25 +47,27 @@ export class MapComponent implements OnInit {
     name: 'P2'
   }, {
     id: 3,
-    latitude: 34.02132363619184, langitud:-6.845276100828468,
+    latitude: 34.02132363619184, langitud: -6.845276100828468,
     nivRemp: 40,
     name: 'P3'
   }, {
     id: 4,
-    latitude: 34.02190020147593, langitud:-6.84476081164375,
+    latitude: 34.02190020147593, langitud: -6.84476081164375,
     nivRemp: 55,
     name: 'P4'
-  },{
+  }, {
     id: 5,
-    latitude: 34.022099742205995, langitud:-6.846452503011824,
+    latitude: 34.022099742205995, langitud: -6.846452503011824,
     nivRemp: 15,
     name: 'P5'
-  },{
+  }, {
     id: 6,
-    latitude: 34.022917829245564, langitud:-6.846109180280347,
+    latitude: 34.022917829245564, langitud: -6.846109180280347,
     nivRemp: 70,
     name: 'P6'
   }];
+
+  listGarbege = [3, 6, 5, 1];
 
   private initMap(): void {
     this.map = L.map('map', {
@@ -68,48 +81,93 @@ export class MapComponent implements OnInit {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
-    // create 5 random jitteries and add them to map
-    /*const jittery = Array(5).fill(this.centroid).map(
-        x => [x[0] + (Math.random() - .5)/10, x[1] + (Math.random() - .5)/10 ]
-      ).map(
-        x => L.marker(x as L.LatLngExpression)
-      ).forEach(
-        x => x.addTo(this.map)
-      );*/
-
-
-
-    //const x=this.listMarkers[1].latitude; const y=  this.listMarkers[1].langitud;
     console.log(this.listMarkers[1].latitude + ',' + this.listMarkers[1].langitud);
-    /*const testMakrker: L.LatLngExpression = [this.listMarkers[1].latitude, this.listMarkers[1].langitud];
-    const x = L.marker(this.centroid);
-    const y = L.marker(testMakrker);
-    y.addTo(this.map);
-    x.addTo(this.map);*/
-    //this.addMarker1([y,x]);
+
+    const marker = L.marker(this.camionPos, { icon: this.iconCamion });
+    marker.addTo(this.map);
+    marker.bindPopup('camion test');
 
     for (let i = 0; i < this.listMarkers.length; i++) {
-      this.addMarker(this.listMarkers[i].latitude,this.listMarkers[i].langitud,i);
-    }
+      this.addMarker(this.listMarkers[i].latitude, this.listMarkers[i].langitud, i, this.icongarbege);
+
+      this.getDistance(L.latLng(this.camionPos[0], this.camionPos[1]),
+        L.latLng(this.listMarkers[i].latitude, this.listMarkers[i].langitud));
+      }
+    console.log("////");
     tiles.addTo(this.map);
 
-    L.Routing.control({
-      waypoints: [
-        L.latLng(this.listMarkers[0].latitude,this.listMarkers[0].langitud),
-        L.latLng(this.listMarkers[4].latitude,this.listMarkers[4].langitud)
-      ]
-    }).addTo(this.map);
-
+    this.drawTrajet(this.listGarbege, this.camionPos);
   }
 
-  addMarker(lat, lang, x){
-    const marker = L.marker([lat, lang], { icon: this.smallIcon });
+  // tslint:disable-next-line:typedef
+  addMarker(lat, lang, x, smallIcon){
+    const marker = L.marker([lat, lang], { icon: smallIcon });
     marker.addTo(this.map);
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup "+x+" .");
+    marker.bindPopup(this.listMarkers[x].name +
+                      '</br>Taux de remplisage :<b>' +
+                      this.listMarkers[x].nivRemp + ' %</b>');
   }
-  addMarker1(latlang){
-    const marker = L.marker(latlang);
-    marker.addTo(this.map);
+
+  // tslint:disable-next-line:typedef
+  getDistance(from, to) {
+    console.log('la distance est :' + from.distanceTo(to));
+
+    return from.distanceTo(to);
+  }
+
+  // tslint:disable-next-line:typedef
+  drawTrajet(listMarkersTodraw, locationCamio){
+    var locationCamioCourant = locationCamio;
+    var listMarkersRestant = listMarkersTodraw;
+    var index = 0;
+    var x=this.getDistance(L.latLng(this.listMarkers[listMarkersRestant[0] - 1].latitude, this.listMarkers[listMarkersRestant[0] - 1].langitud),
+      L.latLng(locationCamioCourant[0], locationCamioCourant[1]));
+    var idGarbege = listMarkersRestant[0];
+
+    console.log('la taille de table : '+listMarkersRestant.length);
+    if(listMarkersRestant.length !=1) {
+      for (let i = 0; i < listMarkersRestant.length; i++) {
+        console.log('--' + listMarkersRestant[i]);
+        let y = this.getDistance(L.latLng(this.listMarkers[listMarkersRestant[i] - 1].latitude, this.listMarkers[listMarkersRestant[i] - 1].langitud),
+          L.latLng(locationCamioCourant[0], locationCamioCourant[1]));
+
+        if (y < x) {
+          idGarbege = listMarkersRestant[i];
+          index = i;
+        }
+      }
+
+      const xx = L.Routing.control({
+        waypoints: [
+          L.latLng(locationCamioCourant[0], locationCamioCourant[1]),
+          L.latLng(this.listMarkers[idGarbege - 1].latitude, this.listMarkers[idGarbege - 1].langitud)
+        ]
+      });
+      xx.addTo(this.map);
+
+      console.log(idGarbege);
+
+      listMarkersRestant.splice(index);
+      locationCamioCourant = [this.listMarkers[idGarbege - 1].latitude, this.listMarkers[idGarbege - 1].langitud];
+    }else{
+      const xx = L.Routing.control({
+        waypoints: [
+          L.latLng(locationCamioCourant[0], locationCamioCourant[1]),
+          L.latLng(this.listMarkers[idGarbege - 1].latitude, this.listMarkers[idGarbege - 1].langitud)
+        ]
+      });
+      xx.addTo(this.map);
+
+      console.log(idGarbege);
+
+      listMarkersRestant.splice(index);
+      locationCamioCourant = [this.listMarkers[idGarbege - 1].latitude, this.listMarkers[idGarbege - 1].langitud];
+
+    }
+    while (listMarkersRestant.length != 0){
+      this.drawTrajet(listMarkersRestant, locationCamioCourant);
+    }
+
   }
 
   constructor() { }
