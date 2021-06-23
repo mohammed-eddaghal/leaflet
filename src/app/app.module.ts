5b41f5deacc4eddb3f33424c4e60ba14e5598a38
fouad el fakhori <58748839+fFakhori@users.noreply.github.com>
2021-06-23 13:28:07 +0100

    Ajouter poubelle et camion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MapComponent } from './components/map/map.component';
+import { PoubelleComponent } from './poubelle/poubelle.component';
+import { CamionComponent } from './camion/camion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    PoubelleComponent,
+    CamionComponent
   ],
   imports: [
     BrowserModule,
